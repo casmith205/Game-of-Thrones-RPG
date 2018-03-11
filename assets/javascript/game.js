e63a1f32a0def5d08b26e2abc568cafad57c15b1
Carolyn Smith <casmith205@gmail.com>
2018-03-11 17:09:46 -0500

    fixed the fade in portion of the reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -146,7 +146,7 @@ function gameReset () {
     playerChoseChar = false;
     enemyCount = (characters.length - 1);
     for(i=0; i<characters.length; i++){
-        $(characters[i]).fadeIn();
+        $(characters[i].buttonId).fadeIn();
     };
     $(".buttons").appendTo(".characters");
 };
